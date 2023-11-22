fd87e2103c8c55f8f0ccea8839e2c5968fc732d1
Xtraxssj <105368588+Xtraxssj@users.noreply.github.com>
2023-11-22 15:01:05 -0700

    Add files via upload

---
+++ eventos/Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://localhost/eventos/";
+const host = "localhost";
+const user = "root";
+const pass = "oscarmtz916";
+const db = "agendas";
+const charset = "charset=utf8";
+?>
